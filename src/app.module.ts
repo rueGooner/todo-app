e2ccb953f7cfa29aba76afa983a59d6b8c3d2f23
rueGooner <ruebencee@gmail.com>
2022-07-11 21:20:07 +0100

    create user resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { TodoModule } from './todo/todo.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, TodoModule],
+  imports: [AuthModule, TodoModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
