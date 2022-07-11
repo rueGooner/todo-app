32c24b781d925d0c96458d80eb081cf60ec794d4
rueGooner <ruebencee@gmail.com>
2022-07-11 20:55:10 +0100

    generate auth module and todo module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, TodoModule],
   controllers: [AppController],
   providers: [AppService],
 })
