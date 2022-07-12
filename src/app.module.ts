8071faa6447b21b700500dde9d9faaaec7cb1352
rueGooner <ruebencee@gmail.com>
2022-07-12 22:38:56 +0100

    use config instead of hard codded url

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
@@ -7,7 +8,15 @@ import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [AuthModule, TodoModule, UserModule, PrismaModule],
+  imports: [
+    AuthModule,
+    TodoModule,
+    UserModule,
+    PrismaModule,
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
